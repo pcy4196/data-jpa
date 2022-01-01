02c65f4c0e78229bfd61f6f89493f2a22718f9dc
PCY <pcy4196@naver.com>
2022-01-01 17:29:53 +0900

    2022.01.01 --  순수JPA 기반 (MemberJpaRepository) 구현

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Spring Data Jpa Practice(2nd) Example
     2. SPRING DATA JPA와 DB설정 -- 중요
 + Ch02. DOMAIN
     1. Member, Team 도메인 설정 및 MemberTest 작성
++ Ch03. INTERFACE
+    1. 순수JPA 기반 (MemberJpaRepository) 구현
--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -16,6 +18,27 @@ public class MemberJpaRepository {
     public Member save(Member member) {
         em.persist(member);
         return member;
+    }
+
+    // 삭제
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        // JPQL(객체를 대상으로 만드는 쿼리)
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult(); // 단건 반환할때 사용
     }
 
     // 조회(아이디)

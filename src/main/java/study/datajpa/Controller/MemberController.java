e7471aac02d083f06e4bd53c4e81b979e400a7a4
PCY <pcy4196@naver.com>
2022-01-31 01:11:28 +0900

    2022.01.31 -- 5. 새로운 엔티티 구분(String --> Persistable<String>)

--- src/main/java/study/datajpa/Controller/MemberController.java
+++ src/main/java/study/datajpa/Controller/MemberController.java
@@ -43,7 +43,7 @@ public class MemberController {
         return memberRepository.findAll(pageable).map(MemberDto::new);
     }
 
-    @PostConstruct
+//    @PostConstruct
     public void init() {
 //        memberRepository.save(new Member("userA"));
         for (int i = 0; i < 100; i++) {
